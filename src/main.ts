de2222deff5c858e3a825cd05de37ad20aa6d4d0
Nada Rifki <nrifki-ext@lequipe.fr>
2022-04-05 20:44:02 +0200

    feat: add confettis

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { createApp } from "vue";
 import { createI18n } from "vue-i18n";
 import { createMetaManager } from "vue-meta";
 import { plugin, defaultConfig } from "@formkit/vue";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import VueConfetti from "vue-confetti";
 
 // Libraries
 import router from "./router";
@@ -29,5 +32,6 @@ app.use(router);
 app.use(store);
 app.use(createI18n());
 app.use(createMetaManager());
+app.use(VueConfetti);
 app.use(plugin, defaultConfig);
 app.mount("#app");
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,11 +4,15 @@
 
 <template>
   <div
-    class="dashboard mx-auto flex max-w-screen-sm flex-col items-center justify-center text-center"
+    class="dashboard mx-auto flex max-w-screen-sm flex-col items-center text-center"
   >
-    <base-logo />
+    <base-logo class="mt-20" />
 
     <h1 class="mt-4 text-xl font-black uppercase text-green-900">MAGMA</h1>
+
+    <div class="mx-auto w-10">
+      <ConfettiExplosion :duration="3000" :force="0.3" :particleCount="200" />
+    </div>
   </div>
 </template>
 
@@ -24,10 +28,12 @@ import { useMeta } from "vue-meta";
 
 // Components
 import BaseLogo from "@/components/base/BaseLogo.vue";
+import ConfettiExplosion from "vue-confetti-explosion";
 
 @Options({
   components: {
     BaseLogo,
+    ConfettiExplosion,
   },
 })
 export default class LaunchIcon extends Vue {
