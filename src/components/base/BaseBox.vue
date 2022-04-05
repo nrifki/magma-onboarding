996b3aa3384ac47c886299a77121dfc6827af0e4
Nada Rifki <nrifki-ext@lequipe.fr>
2022-04-05 23:48:29 +0200

    feat: responsive adjustment

--- src/components/base/BaseBox.vue
+++ src/components/base/BaseBox.vue
@@ -3,7 +3,9 @@
      ************************************************************************* -->
 
 <template>
-  <div class="mx-auto w-96 rounded-2xl border bg-white p-5 text-left shadow-sm">
+  <div
+    class="mx-auto w-72 rounded-2xl border bg-white p-5 text-left shadow-sm sm:w-96"
+  >
     <slot />
   </div>
 </template>
